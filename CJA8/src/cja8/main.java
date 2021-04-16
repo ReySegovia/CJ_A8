4231773f4a655179374e8a7dbd721275fc868e1a
ReySegovia <70732414+ReySegovia@users.noreply.github.com>
2021-04-15 19:04:46 -0500

    Update main.java

--- CJA8/src/cja8/main.java
+++ CJA8/src/cja8/main.java
@@ -4,19 +4,64 @@
  * and open the template in the editor.
  */
 package cja8;
-
+import java.util.Scanner;
 /**
  *
  * @author reysegovia
  */
 
 public class main{     
-    public static void main(String args[]) {         
+    public void showMenu() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Selecciona una opción");
+        System.out.println("1. Mezclar deck");
+        System.out.println("2. Sacar una carta");
+        System.out.println("3. Sacar carta al azar");
+        System.out.println("4. Generar una mano de 5 cartas");
+        System.out.println("0. Salir");
         Deck deck = new Deck();
         deck.deck();     
-        deck.shuffle();  
-        deck.head();     
-        deck.pick();
-        deck.hand();
+        int option = input.nextInt();
+        
+        while (option > 0) {
+            if (option < 1 || option > 4){
+                System.err.println("Opción no válida");
+                System.err.println("Teclea un número del 0 al 4");
+                if (input.hasNextInt())
+                    option = input.nextInt();
+            }
+            
+            else if (option == 1) {
+                deck.shuffle();
+                if (input.hasNextInt())
+                    option = input.nextInt();
+
+            }
+            else if (option == 2) {
+                deck.head();
+                if (input.hasNextInt())
+                    option = input.nextInt();
+
+            }
+            else if (option == 3) {
+                deck.pick();
+                if (input.hasNextInt())
+                    option = input.nextInt();
+            }
+            else if (option == 4) {
+                deck.hand();
+                if (input.hasNextInt())
+                    option = input.nextInt();
+            }
+            else if (option == 0) {
+                break;
+            }
+        }
+    }
+    
+    public static void main(String args[]) {  
+        System.out.println("Bienvenido al Poker");
+        main showMenu = new main();
+        showMenu.showMenu();
     }
 }
